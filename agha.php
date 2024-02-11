2260547c6e4e834220e4edca92291132ca0f7ce5
Clément Nélisse <clementnelisse@iMac.local>
2024-02-11 10:52:06 +0100

    Premier commit

---
+++ agha.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <script></script>
+    <title>Agha</title>
+</head>
+<body>
+    <h1>Agha</h1>
+    <?php
+    ?>
+</body>
+</html>
+/Applications/MAMP/htdocs/agha/agha.php
---
+++ main.php
@@ -0,0 +1,11 @@
+<?php 
+include 'functions.php';
+
+//déclaration
+$cards = [2,3,4,5,6,7,8,9,'Valet','Dame','Roi','As'];
+$colors = ['Carreau', 'Coeur', 'Pique', 'Trèfle'];
+$main = [];
+
+//main 
+
+?>
